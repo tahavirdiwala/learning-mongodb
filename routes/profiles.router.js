8633ee8bce424355153444ba1e8ff5db3068b5f4
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-09-18 18:59:13 +0530

    ...

--- routes/profiles.router.js
+++ routes/profiles.router.js
@@ -1,8 +1,5 @@
-const { getProfiles } = require("../controllers/identifier.controller");
-
 const router = require("express").Router();
 
 router.route("/profiles")
-.get(getProfiles)
 
 module.exports = router
