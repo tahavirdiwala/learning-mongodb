e203bb3419dc06b8266321d49c1d632cbaa3b362
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-09-19 10:49:53 +0530

    implement lookup relation in user with cars

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,7 +4,15 @@ const User = require("../models/users");
 
 const getUsers = async (req, res) => {
   try {
-    const users = await User.find().populate("cars")
+    // const users = await User.find().populate("cars") // relation with populate
+    const users = await User.aggregate([ // relation with aggregate lookup
+      {$lookup: {
+        from : "cars",
+        localField: "cars",
+        foreignField: "_id",
+        as: "cars"
+      }}
+    ])
     sendResponse(res, StatusCodes.OK, "Users Fetched SuccessFully", users);
   } catch (err) {
     sendResponse(res, StatusCodes.BAD_REQUEST, `${err.message}`);
