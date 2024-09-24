9a2bf267684decda515cbe4f1c3a8252019fd5cf
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-09-24 19:05:16 +0530

    ..

--- models/cars.js
+++ models/cars.js
@@ -1,12 +1,16 @@
-const mongoose = require("mongoose"), Schema = mongoose
+const mongoose = require("mongoose"),
+  Schema = mongoose;
 
-const Cars = mongoose.model("Car", new mongoose.Schema({
+const Cars = mongoose.model(
+  "Car",
+  new mongoose.Schema({
     make: String,
     model: String,
     owner: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    }
-}))
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  })
+);
 
-module.exports = Cars
+module.exports = Cars;
--- models/users.js
+++ models/users.js
@@ -1,12 +1,18 @@
-const mongoose = require("mongoose"), Schema = mongoose
+const mongoose = require("mongoose"),
+  Schema = mongoose;
 
-const User = mongoose.model("User", new mongoose.Schema({
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
     username: String,
     email: String,
-    // profile: { type: Schema.Types.ObjectId, ref: 'Profile' }, 
-    cars: [{
-        type: Schema.Types.ObjectId, ref: "Car" // one to many relation
-    }]
-}))
+    cars: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Car", // one to many relation
+      },
+    ],
+  })
+);
 
-module.exports = User
+module.exports = User;
